0c71c5af6183f0fdca6fc16ba9d51734a645ac27
matiasrj <matias@inipop.com>
2022-07-30 09:55:05 -0300

    ADD, applying Decimal instead Float (price-dollar_mep-dollar_cable).

--- challenge-balanz.py
+++ challenge-balanz.py
@@ -1,4 +1,4 @@
-from decimal import DivisionByZero
+from decimal import ROUND_DOWN, ROUND_HALF_UP, Decimal, DivisionByZero
 import msvcrt
 import asyncio
 from time import sleep
@@ -8,11 +8,13 @@ import websockets
 import sys, os
 import json
 
+# Enviro info
 url_list_security_id = "https://test-algobalanz.herokuapp.com/api/v1/prices/security_id"
 url_list_instant_prices = "https://test-algobalanz.herokuapp.com/api/v1/prices"
 url_security_id_detail = "https://test-algobalanz.herokuapp.com/api/v1/prices/security_id/"
 url_websocket_service = 'wss://test-algobalanz.herokuapp.com/ws/generic'
 
+# Initial variables
 instruments = []
 instrument_base_names= []
 
@@ -23,14 +25,28 @@ class Instrument:
         self.price=""
         self.currency=""
         self.setlement_type=""
-        self.dollar_cable=""
-        self.dollar_mep=""
+        self.dollar_cable="0"
+        self.dollar_mep="0"
 
     def __str__(self) -> str:
-        return (self.name + '\t' + str(self.price) + '\t' + self.currency + '\t' + self.setlement_type + "\t"+ str(self.dollar_cable) + "\t"+ str(self.dollar_mep) )
 
+        self.price = Decimal(self.price).quantize(Decimal('.0001'),ROUND_HALF_UP)
+        self.dollar_cable = Decimal(self.dollar_cable).quantize(Decimal('.0001'),ROUND_HALF_UP)
+        self.dollar_mep = Decimal(self.dollar_mep).quantize(Decimal('.0001'),ROUND_HALF_UP)  
+        return (self.name + '\t - \t' + 
+                f'{str(self.price):10}' + '\t - \t' +
+                self.currency + '\t - \t' + 
+                self.setlement_type + "\t - \t"+  
+                f'{str(self.dollar_cable):10}' + '\t - \t' +
+                str(self.dollar_mep)
+                 )
+
+#####################################
+# initialization functions
+#####################################
 
 def init_instruments():
+    # Creation of instruments
     try:
         req_security_ids = requests.get(url_list_security_id)
         req_security_ids_json = json.loads(req_security_ids.text)
@@ -43,7 +59,7 @@ def init_instruments():
         print (e)
 
 def initial_price_instruments():
-    
+    # First Survey of instruments_data
     try:
         req_instant_price_ids = requests.get(url_list_instant_prices)
         req_instant_price_ids_json =  json.loads(req_instant_price_ids.text)
@@ -52,7 +68,7 @@ def initial_price_instruments():
             # Por si no esta en la respuesta
             try:
                 instrument_detail =req_instant_price_ids_json[instrument.name]
-                instrument.price = instrument_detail['last']['price']
+                instrument.price = Decimal(instrument_detail['last']['price'])
                 instrument.setlement_type = instrument_detail["settlementType"]
                 instrument.currency = instrument_detail["currency"]
 
@@ -62,15 +78,19 @@ def initial_price_instruments():
     except Exception as e:
         print (e)
 
-def print_console():
-    for instrument in instruments:
-        print(instrument)
 
+# initialization
 def init():
     init_instruments()
     initial_price_instruments()
 
-# Consumo wss
+
+
+
+
+#####################################
+# Consumo wss functions
+#####################################
 
 
 def buscar_instrument_base ( instrument_ref ):
@@ -98,19 +118,28 @@ def calculate_dollar():
             
             if instrument.currency == "USD":
                 try:
-                    instrument.dollar_cable = instrument_base.price / float(instrument.price)
-                except DivisionByZero as e:
-                    print ('division por 0 en cable')
-                    print("instrument del error: " +instrument)
+                    instrument.dollar_cable = Decimal(instrument_base.price) / Decimal(instrument.price)
+                except Exception as e:
                     pass
             else:
                 try:
-                    instrument.dollar_mep = instrument_base.price / float(instrument.price)
-                except DivisionByZero as e:
-                    print ('division por 0 en mep')
-                    print("instrument del error: " +instrument.name)
+                    instrument.dollar_mep = Decimal(instrument_base.price) / Decimal(instrument.price)
+                except Exception as e:
                     pass
 
+def print_console():
+    print (f'{ "Name":20}' + '\t - \t' + 
+           f'{ "Price":8}'+ '\t - \t' +
+           f'{ "Currency"}'+ ' -   ' +
+           f'{ "Setlement_type"}'+ '  - \t' +
+           f'{ "Dollar_Cable":10}'+ '\t - ' +
+           f'{ "Dollar_mep":10}' + '\n\n')
+    
+
+    for instrument in instruments:
+        print(instrument)
+        
+    print("\n\n=========================================\n\n")
 
 
 def parser_broadcast_msg(msg):
@@ -126,15 +155,17 @@ async def wss():
             try:
                 recv = await websocket.recv()
                 msg = json.loads(recv)["msg"]
-                print(msg)
+                # print(msg)
                 parser_broadcast_msg(msg)
                 calculate_dollar()
                 print_console()
+
+                # Nota: No logre refrescar la console en la misma linea, por lo tanto,
+                # sin sleep, se imprime en consola demasiado rapido, no permite observar 
+                # ( y validar ) los calculos. 
                 sleep(10)
             except Exception as e:
                 print (e)
-
-
 
 def active_wss():    
     loop = asyncio.get_event_loop()
@@ -147,6 +178,9 @@ def active_wss():
         loop.close()
 
 
+#####################################
+# Start APP
+#####################################
 
 
 def main():
